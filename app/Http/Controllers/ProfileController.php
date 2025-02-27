1fba8e5378c355bba42fb76ca784ed965a51800b
ismailbaoud <ibaoud46@gmail.com>
2025-02-27 13:50:53 +0000

    ajouter la gestion de profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,43 +9,16 @@ use Illuminate\Support\Facades\Auth;
 /**
  *
  */
-class profileController extends Controller
+class ProfileController extends Controller
 {
     /**
      * @return void
      */
-    public function index($id){
-        session()->put('user_id', $id);
-        $user = User::find($id);
 
 
-
-        return view('front.profile', compact('user'));
-        }
-
-        public static function store(Request $request){
-            $validatedData = $request->validate([
-                'firstName' => 'required',
-                'lastName' => 'required',
-                'phone' => 'required',
-                'image' => 'required',
-            ]);
-            if ($validatedData['image']) {
-                $imagePath = $request->file('image')->store('profile_images', 'public');
-            } else {
-                $imagePath = null;
-            }
-            User::create([
-                'firstName' => $validatedData['firstName'],
-                'lastName' => $validatedData['lastName'],
-                'account_id' => session('id'),
-                'phone' => $validatedData['phone'] ?? null,
-                'image' => $imagePath,
-            ]);
-
-            return redirect()->route('userDashboard')->with('success', 'Login successful');
-
-        }
-
+    /**
+     * @param ProfileRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
 
 }
