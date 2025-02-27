022900214d6193f31ffaf3219f9d6fde5e6799a3
ismailbaoud <ibaoud46@gmail.com>
2025-02-27 14:58:54 +0000

    le commit final 2

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,7 +14,29 @@ class ProfileController extends Controller
     /**
      * @return void
      */
+    public function show($id){
+        session()->put('user_id', $id);
+        $user = User::find($id);
 
+
+        return view('front.profile', compact('user'));
+    }
+
+
+    public static function store(ProfileRequest $request){
+        $validatedData = $request->validated();
+
+        if ($validatedData['image']) {
+            $imagePath = $request->file('image')->store('profile_images', 'public');
+        } else {
+            $imagePath = null;
+        }
+
+        User::create($validatedData);
+
+        return to_route('userDashboard')->with('success', 'Login successful');
+
+    }
 
     /**
      * @param ProfileRequest $request
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,29 +11,7 @@ use Illuminate\Http\Request;
  */
 class UserController extends Controller
 {
-    public function show($id){
-        session()->put('user_id', $id);
-        $user = User::find($id);
 
-
-        return view('front.profile', compact('user'));
-    }
-
-
-    public static function store(ProfileRequest $request){
-        $validatedData = $request->validated();
-
-        if ($validatedData['image']) {
-            $imagePath = $request->file('image')->store('profile_images', 'public');
-        } else {
-            $imagePath = null;
-        }
-
-        User::create($validatedData);
-
-        return to_route('userDashboard')->with('success', 'Login successful');
-
-    }
     /**
      * @param Request $request
      * @param $id
