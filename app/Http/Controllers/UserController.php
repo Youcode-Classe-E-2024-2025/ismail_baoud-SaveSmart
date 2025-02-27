a6e4d41581283bef5a8a0136ae1097c92bc32b45
ismailbaoud <ibaoud46@gmail.com>
2025-02-27 13:52:45 +0000

    ajouter la gestion des utilisateurs

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -10,32 +11,48 @@ use Illuminate\Http\Request;
  */
 class UserController extends Controller
 {
-    /**
-     * @param $id
-     * @return \Illuminate\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|object
-     */
-    public function deleteUser($id){
+    public function show($id){
+        session()->put('user_id', $id);
         $user = User::find($id);
-        $user->delete();
-        return redirect('/adminDashboard');
+
+
+        return view('front.profile', compact('user'));
     }
 
 
+    public static function store(ProfileRequest $request){
+        $validatedData = $request->validated();
+
+        if ($validatedData['image']) {
+            $imagePath = $request->file('image')->store('profile_images', 'public');
+        } else {
+            $imagePath = null;
+        }
+
+        User::create($validatedData);
+
+        return to_route('userDashboard')->with('success', 'Login successful');
+
+    }
     /**
      * @param Request $request
      * @param $id
      * @return \Illuminate\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|object
      */
-    public function editUser(Request $request , $id){
-
-        $request = $request->validate([
-            'firsName' => 'required|min:3',
-            'lastName' => 'required|min:3',
-            'email' => 'required|email',
-            'phone' => 'required|min:8',
-        ]);
+    public function update(UserRequest $request , $id){
+        $request = $request->validated();
         $user = User::find($id);
+
         $user->update($request);
-        return redirect('/profile');
+        return redirect('/profile/'. $id);
+    }
+    /**
+     * @param $id
+     * @return \Illuminate\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|object
+     */
+    public function destroy($id){
+        $user = User::find($id);
+        $user->delete();
+        return redirect('/adminDashboard');
     }
 }
