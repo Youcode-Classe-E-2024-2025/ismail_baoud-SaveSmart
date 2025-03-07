e04a60354689138a9e2cfbd1bf0f144e2138db41
ismailbaoud <ibaoud46@gmail.com>
2025-03-07 10:56:29 +0000

    chore: Update tests/TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,22 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+
+    /**
+     * Set up the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        // Don't run the parent setup which tries to connect to the database
+        // parent::setUp();
+
+        $this->createApplication();
+    }
 }
