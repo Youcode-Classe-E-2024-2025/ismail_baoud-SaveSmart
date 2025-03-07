b33c2be1437ca33b1001a13ac2c48614d8a637c7
ismailbaoud <ibaoud46@gmail.com>
2025-03-07 10:56:29 +0000

    chore: Update resources/views/components/footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,20 @@
-
-<footer class="bg-black text-white py-6 ">
-    <div class="container mx-auto px-4">
-        <div class="flex justify-center items-center">
-
-                © 2024 saveSmart, Inc. All rights reserved.
-
+<footer class="bg-white border-t border-neutral-200 py-4">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex flex-col md:flex-row justify-between items-center">
+            <div class="flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-primary-600" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
+                    <path fill-rule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clip-rule="evenodd" />
+                </svg>
+                <span class="ml-2 text-sm font-medium text-neutral-600">SaveSmart © 2025</span>
+            </div>
+            <div class="mt-4 md:mt-0">
+                <ul class="flex space-x-4 text-sm text-neutral-500">
+                    <li><a href="#" class="hover:text-primary-600">Privacy Policy</a></li>
+                    <li><a href="#" class="hover:text-primary-600">Terms of Service</a></li>
+                    <li><a href="#" class="hover:text-primary-600">Contact</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </footer>
